cb3ff429418af8a9babcaa8a0a21662c0550cfa6
Unknown <215014128@stu.ukzn.ac.za>
2018-12-20 02:54:03 +0200

    Login Form changed
    feat:only change in the login form component
    refactor:login form is covered using a Card 
    test: unit testing of the login form 
    
    The  login form was modified and  implemented using a card only.

--- src/app/loginform/loginform.component.ts
+++ src/app/loginform/loginform.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Title} from '@angular/platform-browser' ;// merely used for tab broswer naming/title
 import {FormControl,Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import {UserService} from '../user.service';
+
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
@@ -10,14 +11,16 @@ import {UserService} from '../user.service';
 })
 export class LoginformComponent implements OnInit {
 
-  constructor(private router:Router,private user:UserService) {
+  constructor(private router:Router,private user:UserService,private title:Title) {
     
    }
+   store:string;
    hide:boolean=true;// used to mask/show password
    /*Below function show "email" used to the validity and returns null if a control is valid
    or an error object if it's invalid */
    
   ngOnInit() {
+    this.title.setTitle('Home');//display at the top of the tab
   }
   loginuser(capture){
     capture.preventDefault();
@@ -25,7 +28,8 @@ export class LoginformComponent implements OnInit {
     var username=capture.target.elements[0].value;
     var password=capture.target.elements[1].value;
     console.log(username,password);
-
+    this.store=username;
+    // console.log(this.capturee.password);
     if(username=="admin" && password=="12"){
       this.user.setuserloggedin();
       this.router.navigate(['dashboard']);
